6ce687e2a41f808760dbf25d78c3811da872918f
Trevor Mast <trevormast@gmail.com>
2020-04-11 02:48:39 -0700

    style changes

--- app/assets/javascripts/channels/games.js
+++ app/assets/javascripts/channels/games.js
@@ -9,7 +9,5 @@ App.cable = ActionCable.createConsumer();
 App.messages = App.cable.subscriptions.create('GamesChannel', {
   received: function(data) {
     setupTurn(data);
-
-    if (data.winning_token === 'X' || data.winning_token === "O") { alert(data.winning_token + ' Wins!'); }
   }
 });
