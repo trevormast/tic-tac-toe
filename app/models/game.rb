0fc845e005f8a06b40e446ee670f2e54b8583829
Trevor Mast <trevormast@gmail.com>
2020-04-11 00:52:56 -0700

    working on game logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,8 @@
 class Game < ApplicationRecord
   belongs_to :x_user, class_name: 'User'
   belongs_to :o_user, class_name: 'User'
+
+  def active_player
+    grid.count(nil).odd? ? x_user : o_user
+  end
 end
